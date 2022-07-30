f314679a026ba8a6f35883ae7cb338a7679ee0bf
Rahul Shah <rahulsanjay18@gmail.com>
2022-07-29 22:44:36 -0400

    Time: 1225 ms (13.94%), Space: 13.4 MB (99.05%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -13,7 +13,6 @@ class Solution(object):
         while i >= 0 and j <= len(s) and j > i:
             sub = s[i:j]
             if self.isUnique(sub):
-                # print("here")
                 highest = max(highest, j-i)
                 j += 1
             else:
@@ -24,4 +23,24 @@ class Solution(object):
     
     def isUnique(self, sub):
         return len((Counter(sub)).items()) == len(sub)
-            
+    
+    
+    '''
+    class Solution:
+        def lengthOfLongestSubstring(self, s: str) -> int:
+            n = len(s)
+            ans = 0
+            # mp stores the current index of a character
+            mp = {}
+
+            i = 0
+            # try to extend the range [i, j]
+            for j in range(n):
+                if s[j] in mp:
+                    i = max(mp[s[j]], i)
+
+                ans = max(ans, j - i + 1)
+                mp[s[j]] = j + 1
+
+            return ans
+    '''
