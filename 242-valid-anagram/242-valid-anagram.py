6812cc1c22c1da43f5df4b0eade2aaf95c7b4301
Rahul Shah <rahulsanjay18@gmail.com>
2022-07-29 21:02:09 -0400

    Time: 38 ms (92.60%), Space: 15.5 MB (6.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,23 +1,26 @@
 class Solution(object):
-    def word_to_dict(self, word):
-        dic = {}
-        
-        for c in word:
-            if c in dic:
-                dic[c] += 1
-            else:
-                dic[c] = 1
-        
-        return dic
-    
     def isAnagram(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
-        dict_s = self.word_to_dict(s)
-        dict_t = self.word_to_dict(t)
         
-        return dict_s == dict_t
+        if len(s) != len(t):
+            return False
         
+        s_hash = {}
+        t_hash = {}
+        
+        for i in range(len(s)):
+            if s[i] in s_hash:
+                s_hash[s[i]] += 1
+            else:
+                s_hash[s[i]] = 1
+                
+            if t[i] in t_hash:
+                t_hash[t[i]] += 1
+            else:
+                t_hash[t[i]] = 1
+                
+        return s_hash == t_hash
