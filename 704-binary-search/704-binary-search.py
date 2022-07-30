ad496bf27e49ed69ec725234b70915e3002c7992
Rahul Shah <rahulsanjay18@gmail.com>
2022-07-30 12:51:39 -0400

    Time: 266 ms (63.99%), Space: 14.4 MB (94.30%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+        
+        while high >= low:
+            mid = (high + low) // 2
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                return mid
+        
+        return -1
+        
